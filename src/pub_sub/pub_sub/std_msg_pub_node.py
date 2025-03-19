7c127c0b56c137971a0550cc46dbe4f28bba1754
wakodeashay <ashaywakode@gmail.com>
2025-03-19 21:31:38 +0400

    Add std msgs publisher

---
+++ src/pub_sub/pub_sub/std_msg_pub_node.py
@@ -0,0 +1,35 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Bool
+
+class StdMsgPublisher(Node):
+    def __init__(self):
+        super().__init__('std_msgs_publisher')
+        self.geometry_msgs_publisher_ = self.create_publisher(Bool, '/common_topic', 10)
+        self.pub_timer_ = self.create_timer(2.0, self.publish_message)
+        self.count_ = 0
+
+    def publish_message(self):
+        msg = Bool()
+
+        mod_count_ = int(self.count_/10)
+
+        if mod_count_%2 == 0:
+            msg.data = False
+        else:
+            msg.data = True
+
+        self.geometry_msgs_publisher_.publish(msg)
+        self.get_logger().info(f"Published Bool data: {msg.data}")
+
+        self.count_ = self.count_ + 1
+
+def main():
+    rclpy.init()
+    node = StdMsgPublisher()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
