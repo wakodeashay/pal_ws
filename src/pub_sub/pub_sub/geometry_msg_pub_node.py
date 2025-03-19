e39172038a9dbeba4304f8886b25791bdb9e90c8
wakodeashay <ashaywakode@gmail.com>
2025-03-19 21:30:47 +0400

    Add geometry msg publisher

---
+++ src/pub_sub/pub_sub/geometry_msg_pub_node.py
@@ -0,0 +1,28 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Vector3
+
+class GeometryMsgPublisher(Node):
+    def __init__(self):
+        super().__init__('geometry_msgs_publisher')
+        self.geometry_msgs_publisher_ = self.create_publisher(Vector3, '/common_topic', 10)
+        self.pub_timer_ = self.create_timer(2.0, self.publish_message)
+
+    def publish_message(self):
+        msg = Vector3()
+        msg.x = 8.0
+        msg.y = 9.0
+        msg.z = 10.0
+
+        self.geometry_msgs_publisher_.publish(msg)
+        self.get_logger().info(f"Published Vector data: {msg}")
+
+def main():
+    rclpy.init()
+    node = GeometryMsgPublisher()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
