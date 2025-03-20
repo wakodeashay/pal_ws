bac390c9c1a25f2a8ac63ccf787a7b0002ab7c78
wakodeashay <ashaywakode@gmail.com>
2025-03-20 20:26:35 +0400

    Update subscriber node

--- src/pub_sub/pub_sub/sub_node.py
+++ src/pub_sub/pub_sub/sub_node.py
@@ -1,6 +1,38 @@
-def main():
-    print('Hi from pub_sub.')
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from rclpy.subscription import Subscription
+from rosidl_runtime_py.utilities import get_message
 
+class GeneralSubscriber(Node):
+    def __init__(self):
+        super().__init__('general_subscriber')
+
+        topic_name = '/common_topic'
+        topics = self.get_topic_names_and_types()
+
+        for topic, msg_type in topics:
+            self.msg_type = get_message(msg_type[0])
+            if topic_name == topic:
+                self.get_logger().info(f"Topic name {topic_name} available!")
+                self.get_logger().info(f"Subscribing to topic: {topic_name} with type: {msg_type}")
+
+                self.subscription = self.create_subscription(
+                    self.msg_type,
+                    topic_name,
+                    self.subscriber_callback,
+                    QoSProfile(depth=10)
+                )
+
+    def subscriber_callback(self, msg):
+        self.get_logger().info(f"Received message of type {self.msg_type}: {msg}")
+
+def main():
+    rclpy.init()
+    node = GeneralSubscriber()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
